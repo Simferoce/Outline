99f3be4a9eb67913befbb522475e196321ac12a8
le6monstre <le6montre@hotmail.com>
2023-04-28 16:13:29 -0400

    Project

---
+++ Project/Assets/Outline/DepthNormalsFeature.cs
@@ -0,0 +1,15 @@
+using UnityEngine.Rendering.Universal;
+
+public class DepthNormalsFeature : ScriptableRendererFeature
+{
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Create()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
---
+++ Project/Assets/Outline/OutlineFunction.hlsl
@@ -0,0 +1,9 @@
+#ifndef MYHLSLINCLUDE_INCLUDED
+#define MYHLSLINCLUDE_INCLUDED
+
+void MyFunction_float(float3 A, out float3 Out) 
+{
+	Out = A * A;
+}
+
+#endif
